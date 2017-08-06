83ecab07596799c9dbfb2b47f65217c1366e4587
Markus <mk@markuskaiser.com>
2017-08-06 10:36:18 +0000

    Added Mocha/should test frameworks

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var should = require('should');
+var assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      //assert.equal(-1, [1,2,3].indexOf(4));
+      [1,2,3].indexOf(5).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    });
+  });
+});
+
